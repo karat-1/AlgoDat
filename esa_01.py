04ee5eb39869e4218780c6ae758421e4934ac46e
Tarik Gürsoy <tguersoy@protonmail.com>
2022-11-02 18:47:05 +0100

    First Commit

---
+++ esa_01.py
@@ -0,0 +1,86 @@
+"""
+Tarik Gürsoy
+Algorithmen und Datenstrukturen
+ESA1 Aufgabe 1 h)
+Benutzt Python 3.9
+
+Ja, es gibt bessere Lösungen aber nicht ohne einen gewissen Preis (mehr Speicherplatz), oder keinen Leistungszuwachs in Worst-Cases.
+Die Funktion largest_distance_optimized() bricht aus der Schleife heraus sobald largest
+größer ist als der absolute Wert von i - die Länge des Arrays. Wenn wir uns am einen Punkt befinden
+wo es keine größere Distanz mehr geben errechnet werden kann muss die Schleife nicht weiter fortgeführt werden. Außerhalb der beiden
+Worst-Cases (ein Array ohne Duplikate und ein Array aus ausschließlich Duplikaten) wird Zeit gewonnen.
+Da wir hier aber ausschließlich von Worst-Cases ausgehen bleibt die Komplexitätsklasse bei Θ(n^2).
+
+Die Implementation von largest_distance_dict() nutzt eine Hashmap bzw. Dictionary als Datenstruktur welches jede Zahl im
+Eingabearray als Schlüssel nutzt und speichert unter jedem Schlüssel eine Liste der Indexe wo sich die Zahlen
+im Array befinden. Hier müssen wir nur maximal 3x durch das Eingabearray iterieren. Einmal um alle Werte in das
+Dictionary zu schreiben, einmal um durch das Dictionary zu iterieren, und einmal durch die jeweiligen Listen die unter jedem
+Schlüssel im Dictionary abgelegt sind. Die Listen die unter den Schlüssen abgelegt sind, sind jedoch alle zusammen nur so lang wie das Eingabearray.
+Dementsprechend ist die Zeitkomplexität Θ(n).
+"""
+
+
+def largest_distance(n):
+    largest = 0
+    z = n[0]
+    counter = 0
+    inner_counter = 0
+    for i, element_one in enumerate(n):
+        for j, element_two in enumerate(n):
+            counter += 1
+            if n[i] == n[j]:
+                if (j - i) > largest:
+                    inner_counter += 1
+                    largest = (j - i)
+                    z = n[j]
+    print("Schrittzaehler: " + str(counter))
+    print("Innerer Schrittzaehler: " + str(inner_counter))
+    return z
+
+
+def largest_distance_optimized(n):
+    largest = 0
+    z = n[0]
+    counter = 0
+    for i, element_one in enumerate(n):
+        for j, element_two in enumerate(n):
+            counter += 1
+            if n[i] == n[j]:
+                if (j - i) > largest:
+                    largest = (j - i)
+                    z = n[j]
+        # Exitkondition für die Schleife
+        if largest >= abs(i - len(n)):
+            break
+    print("Schrittzaehler: " + str(counter))
+    return z
+
+
+def largest_distance_dict(n: list):
+    number_dict = {}
+    largest = 0
+    z = n[0]
+    counter = 0
+    for i, element in enumerate(n):
+        counter += 1
+        if element in number_dict:
+            number_dict[element].append(i)
+        else:
+            number_dict[element] = [i]
+
+    for key in number_dict:
+        counter += 1
+        indices = number_dict[key]
+        for i, index in enumerate(indices):
+            if i < len(indices) - 1:
+                counter += 1
+                if indices[i + 1] - i > largest:
+                    largest = indices[i + 1] - i
+                    z = key
+    print("Schrittzaehler: " + str(counter))
+    return z
+
+
+input_array = [3, 5, 2, 3, 2, 0, 2, 3, 1, 5]
+input_array_worst_case = [1, 1, 1, 1, 1, 1, 1, 1]
+print("Ausgabewert: " + str((largest_distance_dict(input_array_worst_case))))
---
+++ esa_02.py
@@ -0,0 +1,89 @@
+import random
+
+
+# Helper Functions
+def switch_elements(i: int, j: int, n: list) -> list:
+    n[i], n[j] = n[j], n[i]
+    print('Tausche : ' + str(n[i]) + ' mit ' + str(n[j]))
+    return n
+
+
+def partition(A: list, p: int, r: int) -> int:
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A = switch_elements(i, j, A)
+    A = switch_elements(i + 1, r, A)
+    return i + 1
+
+
+# Sorting Algorithms
+def bubble_sort(n: list) -> list:
+    n_len = len(n)
+    for i in range(n_len - 1):
+        for j in range(0, n_len - i - 1):
+            print('Vergleich: ' + str(n[j]) + ' mit ' + str(n[j + 1]))
+            if n[j] > n[j + 1]:
+                n = switch_elements(j, j + 1, n)
+    return n
+
+
+def quick_sort(A: list, p: int, r: int) -> list:
+    if p < r:
+        q = partition(A, p, r)
+        quick_sort(A, p, q - 1)
+        quick_sort(A, q + 1, r)
+    return A
+
+
+def rek_sort(A: list, start: int, end: int) -> list:
+    m: int = start
+    for i in range(start, end + 1):
+        a = A[i]
+        b = A[m]
+        print('Vergleich: ' + str(a) + ' mit ' + str(b))
+        if a > b:
+            m = i
+    A = switch_elements(m, end, A)
+    if end > start:
+        A = rek_sort(A, start, end - 1)
+    return A
+
+
+def count_pair(n: list) -> list:
+    pairs: dict = {}
+    for i, element in enumerate(n):
+        if element in pairs:
+            pairs[element] += 1
+        else:
+            pairs[element] = 1
+
+    pair_list = []
+    for k, v in pairs.items():
+        pair_list.append((k, v))
+    return pair_list
+
+
+if __name__ == "__main__":
+    # Aufgabe 1 mit allen Ausgaben für a) und b)
+    aufgabe_1_liste = [2, 5, 3, 8, 4, 7]
+    print('Aufgabe 1:')
+    print(bubble_sort(aufgabe_1_liste))
+
+    # Aufgabe 2 mit allen Ausgaben für a) und b)
+    aufgabe_2_liste = [3, 18, 12, 9, 24, 6, 21, 15]
+    print('Aufgabe 2:')
+    print(quick_sort(aufgabe_2_liste, 0, len(aufgabe_2_liste) - 1))
+
+    # Aufgabe 3 mit allen Ausgaben
+    aufgabe_3_liste = [1, 1, 1, 1, 1]
+    print('Aufgabe 3:')
+    print(rek_sort(aufgabe_3_liste, 0, len(aufgabe_3_liste) - 1))
+
+    # Aufgabe 4 mit allen Ausgaben
+    aufgabe_4_liste = []
+    for number in range(0, 100):
+        aufgabe_4_liste.append(random.randint(1, 10))
+    print(count_pair(aufgabe_4_liste))
